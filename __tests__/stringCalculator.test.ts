2ccebd369b2d164a86d0426153ed9c6e49368a20
jeena-thomas <thomasjeena97@gmail.com>
2025-07-29 10:36:49 +0530

    test: support custom delimiters

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -20,3 +20,8 @@ test("should handle new lines between numbers", () => {
     expect(add('1\n2,3')).toBe(6);
     expect(add('1\n2\n3')).toBe(6);
 });
+
+test("should supports custom delimiters", () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//[***]\n1***2***3")).toBe(6);
+});
