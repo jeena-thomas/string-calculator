e9f3da71965662ee0165d8ad6c92723c6a63b2ff
jeena-thomas <thomasjeena97@gmail.com>
2025-07-29 09:28:44 +0530

    chore: initial project setup

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+};
