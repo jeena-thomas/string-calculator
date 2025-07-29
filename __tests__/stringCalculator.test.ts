a6abea4696bd7fe80bb03f22147f67824982a675
jeena-thomas <thomasjeena97@gmail.com>
2025-07-29 10:18:29 +0530

    feat: handle new lines between numbers

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -18,5 +18,5 @@ test("should return sum of multiple comma-separated numbers", () => {
 
 test("should handle new lines between numbers", () => {
     expect(add('1\n2,3')).toBe(6);
-    expect(add('1,\n2\n,3')).toBe(6);
+    expect(add('1\n2\n3')).toBe(6);
 });
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,6 +2,6 @@ export function add(numbers: string): number {
   if (numbers === "") {
     return 0;
   }
-  const parts = numbers.split(',');
-  return parts.reduce((sum, part) => sum + parseInt(part), 0);
+  const parts = numbers.split(/,|\n/).map(num => parseInt(num));
+  return parts.reduce((sum, i) => sum + i, 0);
 }
