99b702e01df3e0a14043cf863608100b80eb65b8
jeena-thomas <thomasjeena97@gmail.com>
2025-07-29 10:46:04 +0530

    test: returns an exception for negative numbers

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -25,3 +25,9 @@ test("should supports custom delimiters", () => {
     expect(add("//;\n1;2")).toBe(3);
     expect(add("//[***]\n1***2***3")).toBe(6);
 });
+
+test("should throw an error for negative numbers", () => {
+  expect(() => add("-1,2")).toThrow("Negative numbers not allowed: -1");
+  expect(() => add("//;\n-6;5")).toThrow("Negative numbers not allowed: -6");
+  expect(() => add("2,-4,3,-5")).toThrow("Negative numbers not allowed: -4,-5");
+});
